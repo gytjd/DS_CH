6f3ec4c7597da51e29f213781ba657cbb8268fb4
Blue <101933437+gytjd@users.noreply.github.com>
2022-04-26 17:25:29 +0900

    Add files via upload
    Binary tree

---
+++ 13/1.c
@@ -0,0 +1,112 @@
+//
+//  1.c
+//  KNU C
+//
+//  Created by hwang hyosung on 2022/04/26.
+//
+
+#include "1.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct node *treePointer;
+typedef struct node {
+    char data;
+    treePointer leftChild;
+    treePointer rightChild;
+}node;
+
+treePointer root=NULL;
+
+treePointer createNode(char data)
+{
+    treePointer new_node=(treePointer)malloc(sizeof(node));
+    new_node->data=data;
+    new_node->leftChild=NULL;
+    new_node->rightChild=NULL;
+    
+    return new_node;
+}
+
+treePointer createBintree(treePointer root,char data)
+{
+    if(root==NULL)
+    {
+        return createNode(data);
+    }
+    
+    if(root->leftChild==NULL)
+    {
+        root->leftChild=createBintree(root->leftChild, data);
+    }
+    else if(root->rightChild==NULL)
+    {
+        root->rightChild=createBintree(root->rightChild, data);
+    }
+    else
+    {
+        if(root->leftChild->leftChild==NULL || root->leftChild->rightChild==NULL)
+        {
+            root->leftChild=createBintree(root->leftChild, data);
+        }
+        
+        else if(root->rightChild->leftChild==NULL || root->rightChild->rightChild==NULL)
+        {
+            root->rightChild=createBintree(root->rightChild, data);
+        }
+        else
+        {
+            root->leftChild=createBintree(root->leftChild, data);
+        }
+    }
+    
+    return root;
+}
+
+void inorder(treePointer root)
+{
+    if(root)
+    {
+        inorder(root->leftChild);
+        printf("%c ",root->data);
+        inorder(root->rightChild);
+    }
+}
+
+void preorder_my(treePointer root)
+{
+    if(root)
+    {
+        printf("%c ",root->data);
+        preorder_my(root->leftChild);
+        
+        preorder_my(root->rightChild);
+    }
+}
+
+void postorder_my(treePointer root)
+{
+    if(root)
+    {
+        postorder_my(root->leftChild);
+        postorder_my(root->rightChild);
+        printf("%c ",root->data);
+    }
+}
+int main()
+{
+    int i;
+    for(i=0;i<5;i++)
+    {
+        root=createBintree(root, 'A'+i);
+    }
+    
+    inorder(root);
+    printf("\n");
+    preorder_my(root);
+    printf("\n");
+    postorder_my(root);
+    printf("\n");
+    return 0;
+}
